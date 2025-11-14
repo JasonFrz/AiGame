a0ac05ede56aad34f71d05dccd5285e7dd46bce3
Hans Christian <152836345+hansckk@users.noreply.github.com>
2025-11-14 13:19:35 +0700

    hhh

--- src/components/GameSection.jsx
+++ src/components/GameSection.jsx
@@ -20,30 +20,24 @@ import cub from "../assets/Leaders_BGA_black_Ourson.png";
 import nemesis from "../assets/Leaders_BGA_black_Nemesis.png";
 
 const GameSection = () => {
-  const [turn, setTurn] = useState(1); //1 (action phase) dan 3(recruitment phase) = human 2 (action phase) dan 4(recruitment phase) = ai
-  const [playerCard, setPlayerCard] = useState(Array(4).fill(null));
-  const [AiCard, setAiCard] = useState(Array(4).fill(null));
-  const [deck, setDeck] = useState([]);
-  const [selectedPiece, setSelectedPiece] = useState(null);
-  const [validMoves, setValidMoves] = useState([]);
   const totalCards = [
-    { name: "Acrobat", img: acrotbat },
-    { name: "Claw Launcher", img: claw_launcher },
-    { name: "Rider", img: rider },
-    { name: "Manipulator", img: manipulator },
-    { name: "Bruiser", img: bruiser },
-    { name: "Royal Guard", img: royal_guard },
-    { name: "Wanderer", img: wanderer },
-    { name: "Illusionist", img: illusionist },
-    { name: "Brewmaster", img: brewmaster },
-    { name: "Archer", img: archer },
-    { name: "Vizier", img: vizier },
-    { name: "Hermit", img: hermit },
-    { name: "Cub", img: cub },
-    { name: "Jailer", img: jailer },
-    { name: "Protector", img: protector },
-    { name: "Assassin", img: assassin },
-    { name: "Nemesis", img: nemesis },
+    { name: "Acrobat", img: acrotbat, board_logo: "A" },
+    { name: "Claw Launcher", img: claw_launcher, board_logo: "CL" },
+    { name: "Rider", img: rider, board_logo: "R" },
+    { name: "Manipulator", img: manipulator, board_logo: "M" },
+    { name: "Bruiser", img: bruiser, board_logo: "B" },
+    { name: "Royal Guard", img: royal_guard, board_logo: "RG" },
+    { name: "Wanderer", img: wanderer, board_logo: "W" },
+    { name: "Illusionist", img: illusionist, board_logo: "I" },
+    { name: "Brewmaster", img: brewmaster, board_logo: "B" },
+    { name: "Archer", img: archer, board_logo: "Ar" },
+    { name: "Vizier", img: vizier, board_logo: "V" },
+    { name: "Hermit", img: hermit, board_logo: "H" },
+    { name: "Cub", img: cub, board_logo: "C" },
+    { name: "Jailer", img: jailer, board_logo: "J" },
+    { name: "Protector", img: protector, board_logo: "P" },
+    { name: "Assassin", img: assassin, board_logo: "As" },
+    { name: "Nemesis", img: nemesis, board_logo: "N" },
   ];
   const initalBoard = [
     ["P2", "P2", "P2", "L2", "P2", "P2", "P2"],
@@ -51,6 +45,18 @@ const GameSection = () => {
     ["T", "T", "T", "T", "T", "T", "T"],
     ["P1", "P1", "P1", "L1", "P1", "P1", "P1"],
   ];
+  const baseBoard = [
+    ["P2", "P2", "P2", "P2", "P2", "P2", "P2"],
+    ["T", "T", "T", "T", "T", "T", "T"],
+    ["T", "T", "T", "T", "T", "T", "T"],
+    ["P1", "P1", "P1", "P1", "P1", "P1", "P1"],
+  ];
+  const [turn, setTurn] = useState(1); //1 (action phase) dan 3(recruitment phase) = human 2 (action phase) dan 4(recruitment phase) = ai
+  const [playerCard, setPlayerCard] = useState(Array(4).fill(null));
+  const [AiCard, setAiCard] = useState(Array(4).fill(null));
+  const [deck, setDeck] = useState([]);
+  const [selectedPiece, setSelectedPiece] = useState(null);
+  const [validMovesHighlight, setValidMovesHighlight] = useState([]);
   const [board, setBoard] = useState(initalBoard);
 
   const shuffledDeck = () => {
@@ -59,13 +65,87 @@ const GameSection = () => {
   };
 
   const handleButtonClick = (row, col, key) => {
+    console.log("Clicked:", row, col, key);
+
+    const isValidMove = validMovesHighlight.some(
+      ([r, c]) => r === row && c === col
+    );
+
+    if (selectedPiece === "L1" && isValidMove) {
+      moveLeader(row, col);
+      return;
+    }
+
     if (key === "L1" && turn === 1) {
-      handleLeaderMove();
+      handleLeaderMove(row, col);
     }
   };
 
-  const handleLeaderMove = () => {
-    alert("leader");
+  const moveLeader = (newRol, newCol) => {
+    const newBoard = board.map((row) => [...row]);
+
+    let oldCol = null;
+    let oldRow = null;
+
+    for (let r = 0; r < newBoard.length; r++) {
+      for (let c = 0; c < newBoard[0].length; c++) {
+        if (newBoard[r][c] === "L1") {
+          oldRow = r;
+          oldCol = c;
+          break;
+        }
+      }
+      if (oldRow !== null) break;
+    }
+
+    newBoard[oldRow][oldCol] = baseBoard[oldRow][oldCol];
+
+    newBoard[newRol][newCol] = "L1";
+
+    setBoard(newBoard);
+    setSelectedPiece(null);
+    setValidMovesHighlight([]);
+  };
+
+  const handleLeaderMove = (row, col) => {
+    if (selectedPiece === "L1") {
+      setSelectedPiece(null);
+      setValidMovesHighlight([]);
+      return;
+    }
+
+    const movesOffset = [
+      [-1, 0], //up
+      [-1, 1], //up-right
+      [-1, -1], //up-left
+      [1, -1], //down-left
+      [1, 1], //down-right
+      [1, 0], //down
+      [0, 1], //right
+      [0, -1], //left
+    ];
+
+    const moves = [];
+
+    movesOffset.forEach(([dr, dc]) => {
+      const newRow = row + dr;
+      const newCol = col + dc;
+
+      if (
+        newRow >= 0 &&
+        newRow < board.length &&
+        newCol >= 0 &&
+        newCol < board[0].length
+      ) {
+        if (board[newRow][newCol] !== "P2" && board[newRow][newCol] !== "L2") {
+          moves.push([newRow, newCol]);
+        }
+      }
+    });
+    setSelectedPiece("L1");
+    setValidMovesHighlight(moves);
+    console.log("Leader at:", row, col);
+    console.log("Valid moves:", moves);
   };
 
   useEffect(() => {
@@ -106,6 +186,7 @@ const GameSection = () => {
             </button>
           ))}
         </div>
+        
         <div className="grid grid-cols-7 grid-row-4 gap-2">
           {board.map((row, rowIndex) =>
             row.map((cell, colIndex) => (
@@ -117,8 +198,14 @@ const GameSection = () => {
                   cell === "P1" || cell === "P2"
                     ? "border-4 border-yellow-400"
                     : ""
-                } bg-white ${
-                  cell === "L2" || cell === "P2" ? "" : "hover:bg-gray-300"
+                } bg-white   ${
+                  validMovesHighlight.some(
+                    ([r, c]) => r === rowIndex && c === colIndex
+                  )
+                    ? "ring-4 ring-green-500 bg-green-300"
+                    : cell === "L2" || cell === "P2"
+                    ? ""
+                    : "hover:bg-gray-300"
                 }`}
               >
                 {cell === "L1" ? (
